8fb0f25ec456d228b5d43d1983399ffbf323d3a3
AlenaAgeeva <alenapvageeva@gmail.com>
2022-10-14 12:59:39 -0300

    Replace all most all Statement on PreparedStatements due to SQL injections warnings. Changed all methods in the PersonDao class. Fully used JDBC api in the project.

--- src/main/java/ru/alishev/springcourse/dao/PersonDao.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDao.java
@@ -3,16 +3,12 @@ package ru.alishev.springcourse.dao;
 import org.springframework.stereotype.Component;
 import ru.alishev.springcourse.models.Person;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDao {
-    private static int ID;
     private final static String URL = "jdbc:postgresql://localhost:5432/spring_mvc_app";
     private final static String USER = "postgres";
     private final static String PASS = "rbnftpsrbnftps2020";
@@ -48,31 +44,61 @@ public class PersonDao {
     }
 
     public Person show(int id) {
-//        return people.stream()
-//                .filter(p -> p.getId() == id)
-//                .findFirst()
-//                .orElse(null);
-        return null;
+        Person person = null;
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("SELECT * from Person where id = ?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            person = new Person(resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("age"),
+                    resultSet.getString("email")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return person;
     }
 
     public void save(Person person) {
         try {
-            connection.createStatement()
-                    .executeUpdate("INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() +
-                            "'," + person.getAge() + ",'" + person.getEmail() + "')");
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO Person VALUES(1,?,?,?)");
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
+            preparedStatement.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id=?");
+            preparedStatement.setString(1, updatedPerson.getName());
+            preparedStatement.setInt(2, updatedPerson.getAge());
+            preparedStatement.setString(3, updatedPerson.getEmail());
+            preparedStatement.setInt(4, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void delete(int id) {
-        //people.removeIf(p -> p.getId() == id);
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("DELETE FROM Person WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
